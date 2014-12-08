3ee97370502987f2c7ff843c432d2ced2879bb69
Jimmy Liao <sjliao@tw.ibm.com>
2014-12-08 23:12:54 +0800

    checkin worklight env

--- adapters/WorklightStarterAdapter/filtered.xsl
+++ adapters/WorklightStarterAdapter/filtered.xsl
@@ -14,20 +14,20 @@
                 xmlns:h="http://www.w3.org/1999/xhtml">
     <xsl:output method="text"/>
     
-<xsl:template match="/rss">
-    {
-        items : [
-            <xsl:for-each select="channel/item">
-                {
-                    title : '<xsl:value-of select="title"/>',
-                    link : '<xsl:value-of select="link"/>',
-                    pubDate : '<xsl:value-of select="pubDate"/>',
-                    description : '<xsl:value-of select="description"/>'
-                },
-            </xsl:for-each>
-        ]
-    }
-</xsl:template>
-
+    <xsl:template match="/">
+        {
+	        'Items': [
+		        <xsl:for-each select="//item">
+	                {
+		                'title': '<xsl:value-of select="title"/>',
+		                'guid': '<xsl:value-of select="guid"/>',
+		                'link': '<xsl:value-of select="link"/>',
+		                'description': '<xsl:value-of select="description"/>',
+		                'pubDate': '<xsl:value-of select="pubDate"/>'
+	                },
+		        </xsl:for-each>
+	        ]
+        }
+    </xsl:template>
 
 </xsl:stylesheet>
--- apps/WorklightStarter/common/js/main.js
+++ apps/WorklightStarter/common/js/main.js
@@ -1,72 +1,3 @@
-var feeds = null;
-
-// jq(".FeedItem").live("click", function(){
-// 	displayFeed(jq(this).attr("id"));
-// });
-
-function loadFeeds(){
-	// jq.mobile.showPageLoadingMsg();
-	var invocationData = {
-			adapter: "WorklightStarterAdapter",
-			procedure: "getEngadgetFeeds",
-			//parameters: [true]
-			parameters: [false]
-	};
-	
-	WL.Client.invokeProcedure(invocationData, {
-		onSuccess: loadFeedsOK, 
-		onFailure: loadFeedsFAIL
-	});
-}
-
-function loadFeedsOK(data){
-	console.log('loadFeedsOK.');
-	if (!data || !data.invocationResult || !data.invocationResult.items || data.invocationResult.items.length==0)
-		showErrorMessage("Could not retrieve feeds");
-	
-	feeds = data.invocationResult.items;
-	jq("#FeedsList").empty();
-	
-	for (var i=0; i<feeds.length; i++){
-		var dataItem = feeds[i];
-		var listItem = jq("<li class='FeedItem' id='" + i + "'><a href='#'><h3>" + dataItem.title + "</h3><p>"+ dataItem.pubDate+"</p></a></li>");
-		jq("#FeedsList").append(listItem);
-	}
-	
-	jq("#FeedsList").listview('refresh');
-	// jq.mobile.hidePageLoadingMsg();
-}
-
-function loadFeedsFAIL(data){
-	showErrorMessage("Server connectivity error");
-}
-
-function displayFeed(feedId){
-	jq.mobile.showPageLoadingMsg();
-	var itemHtml = feeds[feedId].description;
-	var item = jq(itemHtml);
-	
-	jq(item).find("img").each(function(){
-		if (jq(this).attr("src").indexOf("jpg")>=0)
-			jq(this).width(260);
-	});
-	
-	jq("#FeedContent").html(item);
-	jq("#FeedContent a").attr("target","_blank");
-
-	setTimeout(function(){
-		// jq.mobile.hidePageLoadingMsg();
-		jq.mobile.changePage("#FeedContentPage");
-	}, 0);
-	
-}
-
-function showErrorMessage(text){
-	// jq.mobile.hidePageLoadingMsg();
-	jq("#DialogText").html(text);
-	jq.mobile.changePage("#DialogPage");
-}
-
 function wlCommonInit(){
 	/*
 	 * Use of WL.Client.connect() API before any connectivity to a Worklight Server is required. 
@@ -81,6 +12,5 @@ function wlCommonInit(){
 	 */
 	
 	// Common initialization code goes here
-	loadFeeds();
+	
 }
-
